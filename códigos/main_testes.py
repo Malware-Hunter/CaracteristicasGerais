44628bdff06e2c41396f23bf8109a48ea067dc47
SavioSilas <savio.saboia@icomp.ufam.edu.br>
2022-11-11 17:31:45 -0400

    Remoção: INTERNET e ACCESS_NETWORK_STATE

--- códigos/main_testes.py
+++ códigos/main_testes.py
@@ -1,9 +1,8 @@
-import pandas as pd
 import math
+import sys
 from collections import Counter
 from argparse import ArgumentParser
 from utils import get_base_parser, get_dataset, get_X_y
-import sys
 
 def parse_args(argv):
     parser = ArgumentParser(parents=[get_base_parser()])
@@ -22,7 +21,6 @@ def fib(feature):
 
 def fim(feature):
    return len(M[M[feature]==1])/len(M)
-
 """
   Score(fi) = 0 {frequência igual de ocorrência em ambas as classes; sem discriminação}
   Score(fi) ~ 0 {baixa frequência de ocorrência em qualquer uma das classes; pior característica discriminante}
@@ -42,35 +40,52 @@ def entropy(labels): #labels --> RÓTULOS
         entropy -= prob_of_label * math.log2(prob_of_label)
     return entropy
 
-#### Terceira etapa - Non_Frequent_Reduction
+#### Terceira etapa - Information Gain
 def information_gain(data, split_labels):
     info_gain = entropy(data)
     for branched_subset in split_labels:
         info_gain -= len(branched_subset) * entropy(branched_subset) / len(data)
     return info_gain
 
-if __name__=="__main__":
+def get_unique_values(df):
+    for column_name in df.columns:
+        yield (column_name, df[column_name].unique())
 
+def drop_irrelevant_columns(df):
+    # retorna o df sem colunas irrelevantes (colunas com menos de 2 valores possíveis)
+    irrelevant_columns = []
+    for (column_name, unique_values) in get_unique_values(df):
+        if(len(unique_values) < 2):
+            irrelevant_columns.append(column_name)
+    return df.drop(columns=irrelevant_columns)
+
+if __name__=="__main__":
     args = parse_args(sys.argv[1:])
     data = get_dataset(args)
     X, y = get_X_y(args, get_dataset(args))
-
     B = data[(data['class'] == 0)]
     M = data[(data['class'] == 1)]
-
     total_of_benign = len(B)
     total_of_malware = len(M)
 
+    for i in X.columns:
+        if i == 'INTERNET':
+            X = X.drop(columns=['INTERNET'])
+        if i == 'ACCESS_NETWORK_STATE':
+            X = X.drop(columns=['ACCESS_NETWORK_STATE'])
+        X = drop_irrelevant_columns(X)
+    
+    ## corte a partir da característica mais frequente
     ft_sum = X.sum()
     ft_max = ft_sum.max()
     th = ft_max * 0.8
     select_ft = list()
-
     for index, value in ft_sum.items():
         if value >= th:
             select_ft.append(index)
     #print(select_ft)
 
+    ## início do pré-processamento
     print("\nNon-Frequent Reduction --> FREQUÊNCIA DE UMA CARACTERÍSTICA")
     for i in select_ft:
         aux = Non_Frequent_Reduction(i)
